4321c96bfffb0a8a7c25323e15d8c59bf9130050
aman-4321 <amanclash4321@gmail.com>
2024-04-23 17:22:09 +0530

    Completed main components and pages, minor adjustments pending

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from "react";
 import "./App.css";
 import { Routes, BrowserRouter, Route } from "react-router-dom";
 import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
 function App() {
-  <BrowserRouter>
-    <Routes>
-      <Route path="/signup" element={<Signup />}></Route>
-      <Route path="/signin" element={<Sign />}></Route>
-      <Route path="/dashboard" element={<dashboard />}></Route>
-      <Route path="/send" element={<SendMoney />}></Route>
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/signin" element={<Signin />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/send" element={<SendMoney />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
