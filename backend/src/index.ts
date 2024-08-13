f5705a3d2e0f4c5e9b54b01ccac39e4af0786f70
aman-4321 <amanclash4321@gmail.com>
2024-08-13 21:20:47 +0530

    change port for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,10 +4,11 @@ import cors from "cors";
 import { rootRouter } from "./routes";
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
 
-app.listen(3000);
+app.listen(port);
