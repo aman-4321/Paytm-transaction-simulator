70573119cd2f6143e6d24dab964094314ef303cc
aman-4321 <amanclash4321@gmail.com>
2024-09-27 16:18:35 +0530

    updated port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import { rootRouter } from "./routes";
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
