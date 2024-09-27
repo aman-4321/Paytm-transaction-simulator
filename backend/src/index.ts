c4e45648fe883b9fe6f46ef40dac5b2cf88f9b77
aman-4321 <amanclash4321@gmail.com>
2024-09-27 16:44:54 +0530

    added rate limit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,26 @@
 require("dotenv").config();
 import express from "express";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 import { rootRouter } from "./routes";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://paytm.webprojects.live"],
+  }),
+);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 30,
+  message: "Too many requests from this IP, please try again later",
+});
+
+app.use(limiter);
+
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
